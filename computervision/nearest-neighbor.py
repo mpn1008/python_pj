4efeecf7d2606a624c289bc6d8c7fbcfdb1a92e1
irenenguyen <mpn1008@gmail.com>
2025-02-23 13:36:55 +0100

    clean up

--- computervision/nearest-neighbor.py
+++ computervision/nearest-neighbor.py
@@ -3,7 +3,6 @@ from tensorflow.keras import layers, models
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the MNIST dataset
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -11,7 +10,6 @@ mnist = tf.keras.datasets.mnist
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
-# Build the model
 model = models.Sequential([
     layers.Flatten(input_shape=(28, 28)),
     layers.Dense(128, activation='relu'),
@@ -19,22 +17,18 @@ model = models.Sequential([
     layers.Dense(10, activation='softmax')
 ])
 
-# Compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Train the model
 model.fit(x_train, y_train, epochs=5)
 
 # Evaluate the model
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f'\nTest accuracy: {test_acc:.4f}')
 
-# Make predictions
 predictions = model.predict(x_test)
 
-# Display some predictions
 num_images = 5
 plt.figure(figsize=(10, 4))
 for i in range(num_images):
